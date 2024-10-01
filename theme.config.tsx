54ba6c76f621e5077752f3398f09aece8c395b8e
Luca S. <luca@devca.io>
2024-10-01 12:55:52 +0200

    fix logo, fix links

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,12 +1,101 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
-import logo from './assets/devcaLogo.jsx'
 
 const config: DocsThemeConfig = {
-  logo: <logo />,
-  project: {
-    link: 'https://devca.io',
-  },
+  logo: (
+      <>
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            fill="none"
+            viewBox="0 0 556 626"
+        >
+          <path
+              fill="url(#paint0_linear_179_17750)"
+              d="M555.512 312.755c0 172.494-139.856 312.424-312.35 312.424H26.808l96.745-199.947h117.639c44.183 0 206.305-20.296 289.81-233.832.548 1.247 1.072 2.493 1.62 3.765 14.761 36.304 22.89 75.999 22.89 117.59z"
+          ></path>
+          <path
+              fill="url(#paint1_linear_179_17750)"
+              d="M531.001 191.428c-13.415 217.65-245.626 233.832-289.81 233.832 17.429 0 33.986-3.939 48.647-10.996.623-.349 1.247-.623 1.87-.897 36.753-18.501 61.961-56.7 61.961-100.609 0-9.027-.972-17.604-2.942-26.032 1.97-12.517-4.314-24.46-4.314-37.6 0-114.448-77.47-207.802-183.091-236.152C142.751 7.44 128.24.308 105.899.308h137.262c129.483 0 240.54 78.767 287.84 191.095v.025z"
+          ></path>
+          <path
+              fill="url(#paint2_linear_179_17750)"
+              d="M353.717 248.126c0 13.115-1.047 26.056-3.017 38.648-11.719-49.619-56.252-86.547-109.486-86.547H123.55L26.83.33h79.091c22.316 0 43.984 2.942 64.53 8.453 105.621 28.4 183.266 124.82 183.266 239.343z"
+          ></path>
+          <path
+              fill="url(#paint3_linear_179_17750)"
+              d="M.6 312.735l122.95 112.528V200.232L.6 312.735z"
+          ></path>
+          <defs>
+            <linearGradient
+                id="paint0_linear_179_17750"
+                x1="718.905"
+                x2="-13.735"
+                y1="232.242"
+                y2="667.991"
+                gradientUnits="userSpaceOnUse"
+            >
+              <stop stopColor="#472BCB"></stop>
+              <stop offset="1" stopColor="#9747FF"></stop>
+            </linearGradient>
+            <linearGradient
+                id="paint1_linear_179_17750"
+                x1="87.971"
+                x2="580.57"
+                y1="-78.434"
+                y2="545.642"
+                gradientUnits="userSpaceOnUse"
+            >
+              <stop stopColor="#6F53F3"></stop>
+              <stop offset="0.18" stopColor="#6F53F3"></stop>
+              <stop offset="0.34" stopColor="#6F53F3"></stop>
+              <stop offset="0.5" stopColor="#6F53F3"></stop>
+              <stop offset="0.65" stopColor="#6F53F3"></stop>
+              <stop offset="0.79" stopColor="#6F53F3"></stop>
+              <stop offset="0.93" stopColor="#6F53F3"></stop>
+              <stop offset="1" stopColor="#6F53F3"></stop>
+            </linearGradient>
+            <linearGradient
+                id="paint2_linear_179_17750"
+                x1="-75.25"
+                x2="534.016"
+                y1="-1.39"
+                y2="283.283"
+                gradientUnits="userSpaceOnUse"
+            >
+              <stop stopColor="#6644DA"></stop>
+              <stop offset="0.14" stopColor="#6644DA"></stop>
+              <stop offset="0.24" stopColor="#6644DA"></stop>
+              <stop offset="0.49" stopColor="#6644DA"></stop>
+              <stop offset="0.68" stopColor="#6644DA"></stop>
+              <stop offset="0.87" stopColor="#6644DA"></stop>
+              <stop offset="1" stopColor="#6644DA"></stop>
+            </linearGradient>
+            <linearGradient
+                id="paint3_linear_179_17750"
+                x1="-9.748"
+                x2="223.885"
+                y1="250.474"
+                y2="359.636"
+                gradientUnits="userSpaceOnUse"
+            >
+              <stop stopColor="#6644DA"></stop>
+              <stop offset="0.14" stopColor="#6644DA"></stop>
+              <stop offset="0.31" stopColor="#6F53F3"></stop>
+              <stop offset="0.49" stopColor="#6F53F3"></stop>
+              <stop offset="0.68" stopColor="#6F53F3"></stop>
+              <stop offset="0.87" stopColor="#6F53F3"></stop>
+              <stop offset="1" stopColor="#0956A6"></stop>
+              <stop offset="1" stopColor="#6F53F3"></stop>
+            </linearGradient>
+          </defs>
+        </svg>
+        <span style={{marginLeft: '.4em', fontWeight: 800}}>
+        DevCa Docs
+      </span>
+      </>
+  ),
   docsRepositoryBase: 'https://github.com/DevCa-IO/devcaDocs',
   footer: {
     text: 'DevCa, 2024',
